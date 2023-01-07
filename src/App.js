ef5719cc6fc0651dd8cd43d0fb85047c03226b1f
Abdul Hanan Asif <99734167+abdulhananasif@users.noreply.github.com>
2023-01-07 20:30:36 +0500

    feat: components rendered in app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
 import React from 'react';
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  Header,
+  WhatGPT3,
+} from './containers';
+import {CTA, Brand, Navbar} from './components';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
